c8ff6b3618449e89104bd13a84cd40a287f06b4a
leonstegmann <leon.stegmann@tum.de>
2023-11-29 19:30:33 +0100

    ADDED matlab script to create figures

---
+++ createFigureCustom.m
@@ -0,0 +1,32 @@
+%Custom function to graph multiple variables in a single graph.
+%Example on how to use it:
+%t= xvector;                                                        %Define the x-axis vector
+% myvars=[yvector1 yvector2];                                       %Define the y-axis vectors to graph in an array (in this case myvars)
+% mytitle = "$i_{out}$ and reference";                              %Define the title
+% xlab = "Time $[s]$";                                              %Define label for x-axis
+% ylab = "$i_d [A]$";                                               %Define label for y-axis
+% mylegend = ["$i_d$" "Reference"];                                 %Define the legend
+% OPTIONAL ARGUMENT varargin = [ymin,ymax];                                 %Define ylim
+% createFigureCustom(t, myvars,mytitle, xlab, ylab, mylegend);      %Call the function
+
+function myfigure = createFigureCustom(xvar, yvar, mytitle, xlab, ylab, leg, varargin)
+    vars=size(yvar);
+    nvars = vars(2);
+    myfigure = figure;
+    myfigure.Position = [100 100 1280 720];                         %Sets position on screen and dimensions
+    for i = 1:nvars
+        plot(xvar, yvar(:,i),'LineWidth',1.5)
+        hold on
+    end
+        % Process optional arguments
+    if ~isempty(varargin)
+        ylim(varargin{1});
+    end   
+    title(mytitle, 'FontSize',16, 'Interpreter','latex')
+    ylabel(ylab,'FontSize',14, 'Interpreter','latex')
+    xlabel(xlab,'FontSize',14, 'Interpreter','latex')
+    legend(leg,'FontSize',14, 'Interpreter','latex')
+    hold off
+    grid on
+end
+
